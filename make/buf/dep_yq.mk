dd8a1850ecda111f766340398abeee6578723055
Will Roden <will@roden.cc>
2022-02-15 15:24:42 -0600

    use YQ_URL

--- make/buf/dep_yq.mk
+++ make/buf/dep_yq.mk
@@ -11,9 +11,7 @@ YQ := $(CACHE_VERSIONS)/yq/$(YQ_VERSION)
 $(YQ):
 	@rm -f $(CACHE_BIN)/yq
 	@mkdir -p $(CACHE_BIN)
-	curl -ssL --fail \
-		https://github.com/mikefarah/yq/releases/download/$(YQ_VERSION)/yq_$(GOOS)_$(GOARCH) \
-		-o $(CACHE_BIN)/yq
+	curl -ssL --fail $(YQ_URL) -o $(CACHE_BIN)/yq
 	chmod +x $(CACHE_BIN)/yq
 	@rm -rf $(dir $(YQ))
 	@mkdir -p $(dir $(YQ))
