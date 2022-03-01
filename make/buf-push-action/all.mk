5bf2662a7ae4878c6b55223d7d35496333e32f9e
Will Roden <will@roden.cc>
2022-03-01 17:56:52 -0600

    OPEN_CMD

--- make/buf-push-action/all.mk
+++ make/buf-push-action/all.mk
@@ -6,6 +6,10 @@ LICENSE_HEADER_YEAR_RANGE := 2020-2022
 LICENSE_HEADER_IGNORES := \/testdata
 FILE_IGNORES := $(FILE_IGNORES) .build/
 
+# Set a default value for OPEN_CMD so that _assert_var,OPEN_CMD doesn't fail in go.mk if the
+# OS is neither Darwin nor Linux.
+OPEN_CMD := open
+
 include make/go/bootstrap.mk
 include make/go/go.mk
 include make/go/license_header.mk
