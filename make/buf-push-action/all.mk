4a26cceaa9278f64ad1c46972409ac483bd975a8
Will Roden <will@roden.cc>
2022-03-01 17:17:01 -0600

    streamline make

--- make/buf-push-action/all.mk
+++ make/buf-push-action/all.mk
@@ -1,19 +1,14 @@
 GO_BINS := $(GO_BINS) cmd/buf-push-action
-DOCKER_BINS := $(DOCKER_BINS) buf-push-action
 
 LICENSE_HEADER_LICENSE_TYPE := apache
 LICENSE_HEADER_COPYRIGHT_HOLDER := Buf Technologies, Inc.
 LICENSE_HEADER_YEAR_RANGE := 2020-2022
 LICENSE_HEADER_IGNORES := \/testdata
-FILE_IGNORES := $(FILE_IGNORES) \
-	.build/
+FILE_IGNORES := $(FILE_IGNORES) .build/
 
 include make/go/bootstrap.mk
 include make/go/go.mk
-include make/go/docker.mk
-include make/go/buf.mk
 include make/go/license_header.mk
-include make/go/dep_protoc_gen_go.mk
 include make/go/dep_minisign.mk
 
 bufgeneratedeps::
